62544f57b8794a343f911ae625d7c08b00e14c61
Jeffrey Sharpe <jeffreycsharpe1@gmail.com>
2017-06-25 19:33:15 -0500

    Comment out UI selection

--- JPaint/src/main/Main.java
+++ JPaint/src/main/Main.java
@@ -2,6 +2,7 @@ package main;
 
 import controller.IPaintController;
 import controller.JPaintController;
+import view.GuiUiModule.Gui;
 import view.UIFactory;
 import view.UIType;
 import viewInterfaces.InvalidUIException;
@@ -14,11 +15,12 @@ public class Main {
 	public static void main(String[] args) {
 
         try {
-            if(args.length == 0)
+            /*if(args.length == 0)
                 throw new Exception("Must specify ui switch.\nex: \"-ui=[GUI][CMD]\"");
 
             UISelector selector = new UISelector();
-            UIModule ui = selector.getUi(args[0]);
+            UIModule ui = selector.getUi(args[0]); */
+            UIModule ui = new Gui();
             IPaintController controller = new JPaintController(ui);
             JPaint jpaintProgram = new JPaint(controller);
             jpaintProgram.run();

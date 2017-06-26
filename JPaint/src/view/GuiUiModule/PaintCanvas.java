15ca890dc29f7e564f299ca9eaa57ee05ecda2a9
Jeffrey Sharpe <jeffreycsharpe1@gmail.com>
2017-06-25 20:40:52 -0500

    Update Canvas.

--- JPaint/src/view/GuiUiModule/PaintCanvas.java
+++ JPaint/src/view/GuiUiModule/PaintCanvas.java
@@ -1,14 +1,14 @@
 package view.GuiUiModule;
 
 
-import java.awt.Graphics;
+import java.awt.*;
 
 import javax.swing.JComponent;
 
 public class PaintCanvas extends JComponent {
 
 	@Override
-	public void paint(Graphics graphics){
-		
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
 	}
 }
